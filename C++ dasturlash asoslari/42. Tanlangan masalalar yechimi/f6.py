1edb3adca416f6f5e1d15e1f5839b7bd16d3ce37
ilmfan <dasturchi.uzbek@gmail.com>
2021-08-14 08:51:19 +0500

    new file

---
+++ C++ dasturlash asoslari/42. Tanlangan masalalar yechimi/f6.py
@@ -0,0 +1,14 @@
+def prime_numbers(n):
+    output_list = [2]
+    for num in range(3, n+1, 2):
+        divided_into = []
+        does_not_divide = []
+        for i in range(2, num):
+            if num % i == 0:
+                divided_into.append(1)
+            else:
+                does_not_divide.append(1)
+        if len(does_not_divide) == num - 2:
+            output_list.append(num)
+    return output_list
+
